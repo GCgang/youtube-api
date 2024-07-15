fd8a56fe74fe923d7bf93255a1bf14230392c62f
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-16 00:21:29 +0900

    Feat : video detail

--- src/pages/VideoDetail/VideoDetail.jsx
+++ src/pages/VideoDetail/VideoDetail.jsx
@@ -1,8 +1,32 @@
+import { useLocation } from "react-router-dom";
+import ChannelPlayList from "../../components/ChannelPlayList/ChannelPlayList";
+
 export default function VideoDetail() {
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+  console.log(video);
   return (
     <>
-      <div>Video</div>
-      <div>Side Bar</div>
+      <article>
+        <section>
+          <iframe
+            id="player"
+            type="text/html"
+            width="640"
+            height="390"
+            src={`http://www.youtube.com/embed/${video.id}`}
+            style={{ border: "none" }}
+          ></iframe>
+          <h3>{title}</h3>
+          <h4>{channelTitle}</h4>
+          <p>{description}</p>
+        </section>
+        <section>
+          <ChannelPlayList channelId={channelId} />
+        </section>
+      </article>
     </>
   );
 }
